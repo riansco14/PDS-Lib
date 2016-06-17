63fcbafb4505a41317dc9dc68600653d4c42ebc1
riansco14 <rian15sco@hotmail.com>
2016-06-17 15:37:03 -0300

    Refs T90 Foi implementado o controlador da tela do Gerenciador da Biblioteca que será utilizado pelo administrador. Foi feito o Design da tela do Gerenciador da Biblioteca seguindo os parametros da tarefa.

---
+++ src/control/GerenciadorTelaController.java
@@ -0,0 +1,161 @@
+
+package control;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
+import VO.Location;
+import br.com.App.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class GerenciadorTelaController implements Initializable{
+    @FXML     private StackPane workspace;
+    @FXML     private Label lblAdmin;
+    @FXML     private ListView<String> listViewLivro;
+    @FXML     private ListView<String> listViewUsuario;
+    @FXML     private ListView<String> listViewExemplar;
+    @FXML     private ListView<String> listViewEmprestimo;
+    @FXML     private final ObservableList<String> listLivro = FXCollections.observableArrayList("Adicionar Livro",
+            "Manipular Livro");
+    @FXML     private final ObservableList<String> listUsuario = FXCollections.observableArrayList("Adicionar Usuario",
+            "Manipular Usuario");
+    @FXML     private final ObservableList<String> listExemplar = FXCollections.observableArrayList("Adicionar Exemplar",
+            "Manipular Exemplar");
+    @FXML     private final ObservableList<String> listEmprestimo = FXCollections.observableArrayList("Retirar Livro",
+            "Devolver Livro","Emprestimos por Usuario","Emprestimos por Livro");
+    
+    public static String textLabel;
+    
+    private Parent nodeAddLivro;
+    private Parent nodeCRUDLivro;
+    private Parent nodeCRUDUser;
+    private Parent nodeAddUser;
+    private Parent nodeEmprestimoUpdate;
+    private Parent nodeEmprestimoAdicionar;
+    private Parent nodeCRUDEstoque;
+    
+    public void setStageWorkspace (Node node) {
+        workspace.getChildren().setAll(node);
+    }
+    
+   
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        lblAdmin.setText(textLabel);
+        
+        listViewLivro.setItems(listLivro);
+        listViewUsuario.setItems(listUsuario);
+        listViewEmprestimo.setItems(listEmprestimo);
+        listViewExemplar.setItems(listExemplar);
+        
+        try {
+			nodeCRUDLivro=FXMLLoader.load(getClass().getResource(Location.TELA_LIVRO_CRUD.toString()));
+			nodeAddLivro=FXMLLoader.load(getClass().getResource(Location.TELA_LIVRO_CADASTRAR.toString()));
+			nodeCRUDEstoque=FXMLLoader.load(getClass().getResource(Location.TELA_ESTOQUE_CRUD.toString()));
+			
+			nodeAddUser=FXMLLoader.load(getClass().getResource(Location.TELA_USER_CADASTRAR.toString()));
+			nodeCRUDUser=FXMLLoader.load(getClass().getResource(Location.TELA_USER_CRUD.toString()));
+			
+			nodeEmprestimoUpdate=FXMLLoader.load(getClass().getResource(Location.TELA_EMPRESTIMO_DEVOLVER.toString()));
+			nodeEmprestimoAdicionar=FXMLLoader.load(getClass().getResource(Location.TELA_EMPRESTIMO_INSERIR.toString()));
+       
+        } catch (IOException e) {
+			e.printStackTrace();
+		}
+        workspace.setOpacity(100);
+    }
+    
+    
+    @FXML 
+    private void menuItemExit(ActionEvent event) {
+    	Stage stage =(Stage) workspace.getScene().getWindow();
+    	Alert alert=new Alert(AlertType.CONFIRMATION," Bolsonaro",ButtonType.OK,ButtonType.APPLY);
+    	
+    	if(alert.showAndWait().get()==ButtonType.APPLY)
+    		stage.close();
+    }
+    
+    
+    @FXML 
+    private void clickListLivro(MouseEvent event) throws IOException{
+        switch(listViewLivro.getSelectionModel().getSelectedIndex()){
+            case 0:{
+            	setStageWorkspace(nodeAddLivro);
+            }
+            break;
+
+            case 1:{
+            	setStageWorkspace(nodeCRUDLivro);
+            }
+            break;
+        }    
+        
+    }
+    
+    @FXML 
+    private void clickListUsuario(MouseEvent event) throws IOException{
+        switch(listViewUsuario.getSelectionModel().getSelectedIndex()){
+            case 0:{
+            	setStageWorkspace(nodeAddUser);
+            }
+            break;
+
+            case 1:{
+            	setStageWorkspace(nodeCRUDUser);
+            }
+            break;
+        }    
+        
+    }
+    
+    @FXML 
+    private void clickListEmprestimo(MouseEvent event) throws IOException{
+        switch(listViewEmprestimo.getSelectionModel().getSelectedIndex()){
+            case 0:{
+            	setStageWorkspace(nodeEmprestimoAdicionar);
+            }
+            break;
+            
+            case 1:{
+            	setStageWorkspace(nodeEmprestimoUpdate);
+            }
+            break;
+            case 2:{
+            	setStageWorkspace(nodeCRUDEstoque);
+            }
+            break;
+        }    
+        
+    }
+    
+    @FXML 
+    private void clickListExemplar(MouseEvent event) throws IOException{
+        switch(listViewExemplar.getSelectionModel().getSelectedIndex()){
+
+        }    
+        
+    }
+    
+}
